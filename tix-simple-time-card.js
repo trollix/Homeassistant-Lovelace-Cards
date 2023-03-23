788d76ef6a3dfa6a76fb64650c4e508cc3b74aa7
trollix <dr.chandra@laposte.net>
2023-03-23 13:52:16 +0100

    Update tix-simple-time-card.js
    Signed-off-by: trollix <dr.chandra@laposte.net>

--- tix-simple-time-card.js
+++ tix-simple-time-card.js
@@ -1,68 +1,63 @@
 class TixSimpleTimeCard extends HTMLElement {
 
-  setConfig(config) {
-    
-    const defaultConfig = {
-      'use_24h': true,
-      'show_seconds': false,
+    setConfig(config) {
+
+        const defaultConfig = {
+            'use_24h': true,
+            'show_seconds': false,
+        }
+
+        this.config = {
+            ...defaultConfig,
+            ...config
+        };
+
     }
-        
-    this.config = {
-      ...defaultConfig,
-      ...config
-    };
-    /*
-    this.config = config;
-    if (this.config.use_24h == null) this.config.use_h24 = true;
-    if (this.config.show_seconds == null) this.config.hide_seconds = false;
-  }
-  */
-  }
-  
-  getCardSize() {
-    return 1;
-  }
 
-  addZero(i){
-    if (i < 10){
-      i = "0" + i;
+    getCardSize() {
+        return 1;
     }
-    return i;
-  }
 
-  startTime() {
-    var today = new Date(),
-        h = today.getHours(),
-        m = today.getMinutes(),
-        s = today.getSeconds(),
-        p = ( h < 12 ) ? "AM" : "PM";
-
-    m = this.addZero(m);
-    s = this.addZero(s);
-
-    let time_str =  (this.config.use_24h ? h : h % 12 ) + ":" + m +
-                    (this.config.show_seconds ? ":" + s : "" ) +
-                    (this.config.use_24h ? " " : " " + p);
-
-    this.content.innerHTML = time_str;
-
-  }
-
-  set hass(hass) {
-
-    if (!this.content) {
-      const card = document.createElement('HA-card');
-      this.content = document.createElement('div');
-      this.content.style.padding = '16px';
-      this.content.style.fontSize = '4rem';
-      this.style.textAlign = 'center';
-      this.content.style.display = 'inline-block';
-      card.appendChild(this.content);
-      this.appendChild(card);
+    addZero(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
     }
-    this.startTime();
-    setInterval(this.startTime.bind(this), 250);
-  }
+
+    startTime() {
+        var today = new Date(),
+            h = today.getHours(),
+            m = today.getMinutes(),
+            s = today.getSeconds(),
+            p = (h < 12) ? "AM" : "PM";
+
+        m = this.addZero(m);
+        s = this.addZero(s);
+
+        let time_str = (this.config.use_24h ? h : h % 12) + ":" + m +
+            (this.config.show_seconds ? ":" + s : "") +
+            (this.config.use_24h ? " " : " " + p);
+
+        this.content.innerHTML = time_str;
+
+    }
+
+    set hass(hass) {
+
+        if (!this.content) {
+            const card = document.createElement('HA-card');
+            this.content = document.createElement('div');
+            this.content.style.padding = '16px';
+            this.content.style.fontSize = '4rem';
+            this.style.textAlign = 'center';
+            this.content.style.display = 'inline-block';
+            card.appendChild(this.content);
+            this.appendChild(card);
+        }
+        this.startTime();
+        setInterval(this.startTime.bind(this), 250);
+    }
 
 }
 
